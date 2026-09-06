4c336e2dcbe0ea2cf0448f81d548384661560536
TrayePlays <ytpremiumalt24@gmail.com>
2026-09-06 14:45:30 -0400

    Delete Parkour Online BP/scripts directory

--- Parkour Online BP/scripts/dimensions.js
+++
@@ -1,8 +0,0 @@
-import { system } from "@minecraft/server";
-export const dimensions = [];
-system.beforeEvents.startup.subscribe(({ dimensionRegistry }) => {
-    for (let i = 0; i < 5; i++) {
-        dimensionRegistry.registerCustomDimension(`parkour:dimension_${i}`);
-        dimensions.push({ typeId: `parkour:dimension_${i}`, using: false });
-    }
-});
